66cc0e6d7ac4f5ee4c09a543c6b654ee5fa91207
OlleBW <91120009+OlleBW@users.noreply.github.com>
2022-03-14 19:33:00 +0100

    .

---
+++ app.js
@@ -0,0 +1,16 @@
+let click = 0;
+
+let label = document.querySelector(".pointsText", )
+
+let label2 = document.querySelector(".donutButton")
+
+label2.addEventListener()
+
+function handleclick(){
+    click += 1;
+    label.innerHTML = 
+}
+
+const donutButton = document.querySelector("#donut-action")
+donutButton.addEventListener("click", points);
+
