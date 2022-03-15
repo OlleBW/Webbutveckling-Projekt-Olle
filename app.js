b26d82c80604ae42f82fef22df3a112f07e30169
OlleBW <91120009+OlleBW@users.noreply.github.com>
2022-03-15 11:45:12 +0100

    .

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 let click = 0;
 
-let label = document.querySelector(".pointsText", )
+let pointsText = document.querySelector("pointsText");
 
-let label2 = document.querySelector(".donutButton")
-
-label2.addEventListener()
-
-function handleclick(){
-    click += 1;
+let button = document.querySelector(".donut-btn");
+button.addEventListener("click", handleClick);
+function handleClick() {
+  click++;
+  pointsText.innerHTML = click;
 }
-
-const donutButton = document.querySelector("#donut-action")
-donutButton.addEventListener("click", points);
-
