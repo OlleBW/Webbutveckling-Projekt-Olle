5ffa4ae6ce4ebc4a1c85f0da4cac844b0ddca824
OlleBW <91120009+OlleBW@users.noreply.github.com>
2022-03-16 14:40:34 +0100

    .

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 let click = 0;
 let clickPower = 1;
+let clickPowerCost = 10;
+let autoClickerCost = 10;
 
 let button = document.querySelector(".donut-btn");
 let div = document.querySelector(".points");
 button.addEventListener("click", handleClick);
 function handleClick() {
-  click++;
+  click += clickPower;
   div.innerHTML = click;
 }
 
 let button1 = document.querySelector(".autoClicker");
 button1.addEventListener("click", donutTimer);
-function donutTimer (){
-  setInterval(giveAutoclick, 700);
+function donutTimer() {
+  if (click >= autoClickerCost) {
+    click -= autoClickerCost;
+    div.innerHTML = click;
+    autoClickerCost = autoClickerCost*2;
+    autoClicker.innerHTML = "Auto-Clicker (Cost: 10)"
+    setInterval(giveAutoclick, 500);
+  }
 }
 function giveAutoclick() {
   click++;
   div.innerHTML = click;
+}
+
+let button2 = document.querySelector(".clickPowerup");
+button2.addEventListener("click", pwrUp);
+function pwrUp() {
+  if(click >= clickPowerCost){
+    clickPower += 2;
+    click -= clickPowerCost;
+    div.innerHTML = click;
+  }
 }
