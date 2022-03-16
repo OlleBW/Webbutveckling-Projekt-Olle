395ba804335e6699f72fb773fc474be1e1f45ff4
OlleBW <91120009+OlleBW@users.noreply.github.com>
2022-03-16 10:09:42 +0100

    .

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 let click = 0;
+let clickPower = 1;
 
 let button = document.querySelector(".donut-btn");
 let div = document.querySelector(".points");
 button.addEventListener("click", handleClick);
 function handleClick() {
+  click++;
+  div.innerHTML = click;
+}
+
+let button1 = document.querySelector(".autoClicker");
+button1.addEventListener("click", donutTimer);
+function donutTimer (){
+  setInterval(giveAutoclick, 700);
+}
+function giveAutoclick() {
   click++;
   div.innerHTML = click;
 }
