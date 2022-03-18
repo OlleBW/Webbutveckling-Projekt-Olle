20e9414d62e3637e7b1e40ca0c1ba8d8c1bf4102
OlleBW <91120009+OlleBW@users.noreply.github.com>
2022-03-18 23:03:36 +0100

    .

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 let donuts = 0;
 let clickPower = 1;
 let clickPowerCost = 10;
-let pwrBtn = document.querySelector(".clickPowerup")
 let autoClickerCost = 10;
-let autoBtn = document.querySelector(".autoClicker")
+let gambleCost = 26;
+
+let negativeReturn = 0;
+let positiveReturn = 1;
+let Jackpot = 4;
+
 
 let button = document.querySelector(".donut-btn");
 let div = document.querySelector(".points");
@@ -13,14 +17,14 @@ function handleClick() {
   div.innerHTML = donuts;
 }
 
-let button1 = document.querySelector(".autoClicker");
-button1.addEventListener("click", donutTimer);
+let autoBtn = document.querySelector(".autoClicker");
+autoBtn.addEventListener("click", donutTimer);
 function donutTimer() {
   if (donuts >= autoClickerCost) {
     donuts -= autoClickerCost;
     div.innerHTML = donuts;
-    autoClickerCost = autoClickerCost*2;
-    autoBtn.innerHTML = `Auto-Clicker (Cost: ${autoClickerCost})`
+    autoClickerCost = autoClickerCost * 2;
+    autoBtn.innerHTML = `Auto-Clicker (Cost: ${autoClickerCost})`;
     setInterval(giveAutoclick, 500);
   }
 }
@@ -29,14 +33,24 @@ function giveAutoclick() {
   div.innerHTML = donuts;
 }
 
-let button2 = document.querySelector(".clickPowerup");
-button2.addEventListener("click", pwrUp);
+let pwrBtn = document.querySelector(".clickPowerup");
+pwrBtn.addEventListener("click", pwrUp);
 function pwrUp() {
-  if(donuts >= clickPowerCost){
+  if (donuts >= clickPowerCost) {
     clickPower += 2;
     donuts -= clickPowerCost;
     div.innerHTML = donuts;
-    clickPowerCost = clickPowerCost*3;
-    pwrBtn.innerHTML = `Click-Powerup (Cost: ${clickPowerCost})`
+    clickPowerCost = clickPowerCost * 3;
+    pwrBtn.innerHTML = `Click-Powerup (Cost: ${clickPowerCost})`;
+  }
+}
+
+let gambleBtn = document.querySelector(".gambleBtn");
+gambleBtn.addEventListener("click", gamble);
+function gamble() {
+  if (donuts >= gambleCost) {
+    // donuts -= gambleCost;
+    // div.innerHTML = donuts;
+    let gambleNumber = Math.floor(Math.random() * 4);
   }
 }
